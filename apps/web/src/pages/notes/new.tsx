5413b48c52407ea5c902d5fc7c913829cd09225c
abehidek <hidek.abe@outlook.com>
2023-03-22 11:20:04 -0300

    feat: new note markdown editor

--- apps/web/src/pages/notes/new.tsx
+++ apps/web/src/pages/notes/new.tsx
@@ -1,10 +1,90 @@
 import { env } from "env";
 import { BaseLayout } from "../../components/BaseLayout"
+import { OptionalLayout } from "../../components/AuthLayout";
+import { Navigate } from "react-router-dom";
+import { RichTextEditor, Link } from '@mantine/tiptap';
+import { useEditor } from '@tiptap/react';
+import Highlight from '@tiptap/extension-highlight';
+import StarterKit from '@tiptap/starter-kit';
+import Underline from '@tiptap/extension-underline';
+import TextAlign from '@tiptap/extension-text-align';
+import Superscript from '@tiptap/extension-superscript';
+import SubScript from '@tiptap/extension-subscript';
+import Placeholder from '@tiptap/extension-placeholder';
+import { useState } from "react";
 
 const NewNote: React.FC = () => {
+  const [content, setContent] = useState('')
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Underline,
+      Link,
+      Superscript,
+      SubScript,
+      Highlight,
+      Placeholder.configure({ placeholder: 'Draft here...' }),
+      TextAlign.configure({ types: ['heading', 'paragraph'] }),
+    ],
+    content,
+    onUpdate: (props) => {
+      setContent(props.editor.getHTML())
+    }
+  });
   return (
     <BaseLayout href="/notes/new" title="New">
-      <h3>Add a new note</h3>
+      <OptionalLayout>
+        {(user) => {
+          if (!user) return <Navigate to="/" />
+          return (
+            <div>
+              <RichTextEditor editor={editor} styles={{ root: { border: 0 } }} >
+                <RichTextEditor.Toolbar sticky stickyOffset={0}>
+                  <RichTextEditor.ControlsGroup>
+                    <RichTextEditor.Bold />
+                    <RichTextEditor.Italic />
+                    <RichTextEditor.Underline />
+                    <RichTextEditor.Strikethrough />
+                    <RichTextEditor.ClearFormatting />
+                    <RichTextEditor.Highlight />
+                    <RichTextEditor.Code />
+                  </RichTextEditor.ControlsGroup>
+
+                  <RichTextEditor.ControlsGroup>
+                    <RichTextEditor.H1 />
+                    <RichTextEditor.H2 />
+                    <RichTextEditor.H3 />
+                    <RichTextEditor.H4 />
+                  </RichTextEditor.ControlsGroup>
+
+                  <RichTextEditor.ControlsGroup>
+                    <RichTextEditor.Blockquote />
+                    <RichTextEditor.Hr />
+                    <RichTextEditor.BulletList />
+                    <RichTextEditor.OrderedList />
+                    <RichTextEditor.Subscript />
+                    <RichTextEditor.Superscript />
+                  </RichTextEditor.ControlsGroup>
+
+                  <RichTextEditor.ControlsGroup>
+                    <RichTextEditor.Link />
+                    <RichTextEditor.Unlink />
+                  </RichTextEditor.ControlsGroup>
+
+                  <RichTextEditor.ControlsGroup>
+                    <RichTextEditor.AlignLeft />
+                    <RichTextEditor.AlignCenter />
+                    <RichTextEditor.AlignJustify />
+                    <RichTextEditor.AlignRight />
+                  </RichTextEditor.ControlsGroup>
+                </RichTextEditor.Toolbar>
+
+                <RichTextEditor.Content />
+              </RichTextEditor>
+            </div>
+          )
+        }}
+      </OptionalLayout>
     </BaseLayout>
   )
 }
